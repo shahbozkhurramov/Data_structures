8deb87af1ad4b8203c88da32468f0eb29d1e13f1
shahbozkhurramov <shahbozkhurramov@gmail.com>
2022-03-30 15:12:59 +0500

    Insert node to with index

--- LinkedListSample.cs
+++ LinkedListSample.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace Data_structures;
 
 public class Node<T>
@@ -13,7 +15,7 @@ public class Node<T>
     }
 }
 
-public class LinkedListSample<T>
+public class LinkedListSample<T> : IEnumerable<T>
 {
     public Node<T> Head { get; set; }
     
@@ -36,4 +38,46 @@ public class LinkedListSample<T>
         }
         Count++;
     }
+
+    public void InsertNode(int index, T data)
+    {
+        Node<T> node = new(data);
+        if(index == 0)
+        {
+            node.Next = Head;
+            Head = node;
+        }
+
+        if(index < Count)
+        {
+            var temp = Head;
+            int index1 = 1;
+            while(temp != null)
+            {
+                if(index == index1)
+                {
+                    var temp1 = temp.Next;
+                    temp.Next = node;
+                    node.Next = temp1;
+                }
+
+                index1++;
+                temp  = temp.Next;
+            }
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var node = Head;
+        while(node != null)
+        {
+            var data = node.Data;
+            node  = node.Next;
+            yield return data;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,15 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        
+        LinkedListSample<int> list = new();
+        list.AddNode(1);
+        list.AddNode(4);
+        list.AddNode(5);
+        list.AddNode(7);
+        list.InsertNode(4, 10);
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
